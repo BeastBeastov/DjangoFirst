f77e8bb6267fe8f175bbaaf13385eb9f58273ebb
BeastDescktop <7710963@mail.ru>
2023-04-07 16:19:05 +0300

    Добавлена проверка орфографии

--- djangoblog/main/forms.py
+++ djangoblog/main/forms.py
@@ -10,16 +10,20 @@ class ArticleForm(ModelForm):
             'title': TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Введите название статьи',
+                'spellcheck': 'True'
             }),
             'intro': Textarea(attrs={
                 'class': 'form-control',
                 'rows': '2',
-                'placeholder': 'Введите анонс статьи'
+                'placeholder': 'Введите анонс статьи',
+                'spellcheck': 'True'
+
             }),
             'text': Textarea(attrs={
                 'class': 'form-control',
                 'rows': '7',
-                'placeholder': 'Введите текст статьи'
+                'placeholder': 'Введите текст статьи',
+                'spellcheck': 'True'
             }),
         }
 
